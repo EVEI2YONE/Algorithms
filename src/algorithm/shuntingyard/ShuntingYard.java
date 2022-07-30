1d22cb36e6767b6be91f013e529f1ad7483e078c
azva_ <skenneth@asu.edu>
2022-07-30 15:54:34 -0700

    Added comments and removed additional functionality

--- src/algorithm/shuntingyard/ShuntingYard.java
+++ src/algorithm/shuntingyard/ShuntingYard.java
@@ -18,13 +18,13 @@ public class ShuntingYard {
         boolean exponentFound = false;
         int skip = 0;
         for(char c : input.toCharArray()) {
-            if(skip > 0) {
+            if(skip > 0) { //this is to skip nested values '(7-4)' = 4 skips since currently point to '('
                 skip--;
                 continue;
             }
             if(Character.isLetter(c) || Character.isDigit(c))
                 result.append((c));
-            else if(IsOperand(c)) {
+            else if(IsOperand(c)) { //Compare current precedence to current operator
                 if(exponentFound) {
                     result.append('^');
                     exponentFound = false;
@@ -38,13 +38,13 @@ public class ShuntingYard {
                 }
 
                 if(state == State.CHANGE) {
-                    AppendOperandStack(result, stack);
+                    AppendOperandStack(result, stack); //empty operand stack and start over
                     state = State.LOW; //high -> low (reset)
                 }
-                stack.append((c));
+                stack.append((c)); //push to operand stack
             }
             else if(IsNested(c)) {
-                String extractNested = ExtractNestedShunt(input, c,true); //remove it as well
+                String extractNested = ExtractNestedShunt(input, c); //remove it as well
                 skip = extractNested.length()+1;
                 result.append(Shunt(extractNested));
             }
@@ -56,16 +56,12 @@ public class ShuntingYard {
         return result;
     }
 
-    private String ExtractNestedShunt(String input, char open, boolean remove) {
+    private String ExtractNestedShunt(String input, char open) {
         StringBuilder sbInput = new StringBuilder(input);
         int start = input.indexOf(open);
         char close = FindPair(input, open);
         int end = input.indexOf(close);
-        String result = sbInput.substring(start+1, end);
-
-        if(remove);
-            sbInput.delete(start, end+1);
-        return result;
+        return sbInput.substring(start+1, end);
     }
 
     private char FindPair(String input, char open) {
